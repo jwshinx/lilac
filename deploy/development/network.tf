bc79b38c6fe3a1fde1694272445532962723c431
Joel Shin <joel@fluxxlabs.com>
2021-01-18 10:13:03 -0800

    created details to public subnets

--- deploy/development/network.tf
+++ deploy/development/network.tf
@@ -9,6 +9,16 @@ resource "aws_vpc" "main" {
   )
 }
 
+# Create an internet gateway to give our subnet access to the outside world
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    local.common_tags,
+    map("Name", "${local.project}-${local.prefix}-ig")
+  )
+}
+
 #############################################################
 # public subnets a and b - inbound/outbound internet access #
 #############################################################
@@ -24,11 +34,94 @@ resource "aws_subnet" "public_a" {
   )
 }
 
+# Grant the VPC internet access on its main route table
+resource "aws_route_table" "public_a" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    local.common_tags,
+    map("Name", "${local.project}-${local.prefix}-public-a")
+  )
+}
+
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public_a.id
+}
+
+# Grant the VPC internet access on its main route table
+resource "aws_route" "public_internet_access_a" {
+  route_table_id         = aws_route_table.public_a.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+}
+
+# eip required when need to communicate with internet
+resource "aws_eip" "public_a" {
+  vpc = true
+
+  tags = merge(
+    local.common_tags,
+    map("Name", "${local.project}-${local.prefix}-public-a")
+  )
+}
+
+resource "aws_nat_gateway" "public_a" {
+  allocation_id = aws_eip.public_a.id
+  subnet_id     = aws_subnet.public_a.id
+
+  tags = merge(
+    local.common_tags,
+    map("Name", "${local.project}-${local.prefix}-public-a")
+  )
+}
+
 resource "aws_subnet" "public_b" {
   cidr_block              = "10.1.2.0/24"
   map_public_ip_on_launch = true
   vpc_id                  = aws_vpc.main.id
   availability_zone       = "${data.aws_region.current.name}b"
+
+  tags = merge(
+    local.common_tags,
+    map("Name", "${local.project}-${local.prefix}-public-b")
+  )
+}
+
+# Grant the VPC internet access on its main route table
+resource "aws_route_table" "public_b" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    local.common_tags,
+    map("Name", "${local.project}-${local.prefix}-public-b")
+  )
+}
+
+resource "aws_route_table_association" "public_b" {
+  subnet_id      = aws_subnet.public_b.id
+  route_table_id = aws_route_table.public_b.id
+}
+
+resource "aws_route" "public_internet_access_b" {
+  route_table_id         = aws_route_table.public_b.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+}
+
+# eip required when need to communicate with internet
+resource "aws_eip" "public_b" {
+  vpc = true
+
+  tags = merge(
+    local.common_tags,
+    map("Name", "${local.project}-${local.prefix}-public-b")
+  )
+}
+
+resource "aws_nat_gateway" "public_b" {
+  allocation_id = aws_eip.public_b.id
+  subnet_id     = aws_subnet.public_b.id
 
   tags = merge(
     local.common_tags,
