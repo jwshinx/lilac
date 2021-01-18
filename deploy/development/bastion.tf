59131a4fa6150021cc06bb2901aa2c775c484c3f
Joel Shin <joel@fluxxlabs.com>
2021-01-18 13:03:37 -0800

    added user data script for bastion

--- deploy/development/bastion.tf
+++ deploy/development/bastion.tf
@@ -31,7 +31,7 @@ data "aws_ami" "amazon_linux" {
 resource "aws_instance" "bastion" {
   ami           = data.aws_ami.amazon_linux.id
   instance_type = "t2.micro"
-  # user_data            = file("./templates/bastion/user-data.sh")
+  user_data            = file("./templates/bastion/user-data.sh")
   # iam_instance_profile = aws_iam_instance_profile.bastion.name
   # key_name             = var.bastion_key_name
   # subnet_id            = aws_subnet.public_a.id
